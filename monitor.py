7a1e177e2c45d683fcf111a65e84fa67015268f8
tehblasian <jeremiahdavid.wreh@gmail.com>
2019-10-07 15:39:02 -0400

    Begin http monitor

---
+++ monitor.py
@@ -0,0 +1,17 @@
+from typing import Type, List
+from logkeep import LogKeep
+from traffic_statistic import TrafficStatistic
+
+class HTTPLogMonitor:
+    def __init__(self, logkeep: Type[LogKeep], alerter, traffic_stats: List[Type[TrafficStatistic]]):
+        self.logkeep = logkeep
+        self.alerter = alerter
+        self.traffic_stats = traffic_stats
+
+    def calculate_stats(self):
+        recent_loglines = self.logkeep.read_recent_loglines()
+        for statistic in self.traffic_stats:
+            statistic.calculate_statistic()
+
+    def get_alert(self):
+        pass
